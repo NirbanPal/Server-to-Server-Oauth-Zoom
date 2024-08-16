624164fa46e118f9281f43fcdec99a6e64449e38
Nirban Pal <90189572+NirbanPal@users.noreply.github.com>
2024-08-16 22:24:19 +0530

    Update zoom.py

--- zoom.py
+++ zoom.py
@@ -2,13 +2,13 @@ import requests
 import json
 
 # replace with your client ID
-client_id = "t_XIL2UNS2KTBYi9KmCvNQ"
+client_id = ""
 
 # replace with your account ID
-account_id = "F8FPt04CSSiKGXJxg4R5sQ"
+account_id = ""
 
 # replace with your client secret
-client_secret = "rPLTJvqyQspKt7HDCvcLsKJQdX7M4P5g"
+client_secret = ""
 
 auth_token_url = "https://zoom.us/oauth/token"
 api_base_url = "https://api.zoom.us/v2"
@@ -103,82 +103,3 @@ zoommeeting=Zoommeeting(client_id,account_id,client_secret)
 # zoommeeting.GetMeetingInfo('73855538968')
 # zoommeeting.DeleteMeeting('73855538968')
 
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-# create the Zoom link function
-
-
-# def create_meeting(topic, duration, start_date, start_time,password):
-#     data = {
-#         "grant_type": "account_credentials",
-#         "account_id": account_id,
-#         "client_secret": client_secret
-#     }
-#     response = requests.post(auth_token_url,
-#                              auth=(client_id, client_secret),
-#                              data=data)
-
-#     if response.status_code != 200:
-#         print("Unable to get access token")
-#     response_data = response.json()
-#     access_token = response_data["access_token"]
-
-#     headers = {
-#         "Authorization": f"Bearer {access_token}",
-#         "Content-Type": "application/json"
-#     }
-#     payload = {
-#         "topic": topic,
-#         "duration": duration,
-#         'start_time': f'{start_date}T10:{start_time}',
-#         "type": 2,
-#         "password":password,
-#         'settings':{
-#             'join_before_host': True
-#         }
-#     }
-
-#     resp = requests.post(f"{api_base_url}/users/me/meetings",
-#                          headers=headers,
-#                          json=payload)
-
-#     if resp.status_code != 201:
-#         print("Unable to generate meeting link")
-#     response_data = resp.json()
-
-#     content = {
-#         "meeting_url": response_data["join_url"],
-#         "password": response_data["password"],
-#         "meetingTime": response_data["start_time"],
-#         "purpose": response_data["topic"],
-#         "duration": response_data["duration"],
-#         "message": "Success",
-#         "status": 1
-#     }
-#     print(response_data)
-
-
-
-
-# create_meeting(
-#       "Test Zoom Meeting",
-#       "60",
-#       "2023-09-24",
-#       "18:24",
-#       "sitaraman"
-#       )
